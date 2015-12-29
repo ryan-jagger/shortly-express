4772374e02e55af877081633fd034aa247d7276a
Ryan Jagger <ryanjagger@gmail.com>
2015-12-29 14:02:24 -0600

    Users table schema defined and working

--- app/config.js
+++ app/config.js
@@ -41,6 +41,20 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
   }
 });
 
+
+db.knex.schema.hasTable('users').then(function(exists){
+  if(!exists) {
+    db.knex.schema.createTable('users', function(user){
+      user.string('username', 255).primary();
+      user.string('password', 255);
+    }).then(function(table){
+      console.log("Created Table ", table);
+    });
+  }
+});
+
+
+
 /************************************************************/
 // Add additional schema definitions below
 /************************************************************/
